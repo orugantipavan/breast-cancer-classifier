54f006b2730039d5e08c73e8c87f47bd670c1be7
orugantipavan <orugantipavan21@gmail.com>
2026-03-25 01:52:04 +0530

    Add Flask API for prediction

---
+++ src/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+#Initailize app
+app = Flask(__name__)
+
+#Load model and scaler
+model = joblib.load("models/model.pkl")
+scaler = joblib.load("models/scaler.pkl")
+
+print("Model and scaler loaded")
+
+#Home route
+@app.route("/")
+def home():
+    return "Breast Cancer Prediction API is running"
+
+#Prediction route
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+
+        #Expecting input as list of 30 features
+        features = np.array(data["features"]).reshape(1, -1)
+
+        #Scale input
+        features_scaled = scaler.transform(features)
+        
+        #Predict
+        prediction = model.predict(features_scaled)[0]
+
+        result = "Benign" if prediction == 1 else "Malignant"
+
+        return jsonify({"prediction": int(prediction), "result": result})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+    #Run server
+if __name__ == "__main__":
+    app.run(debug=True)
