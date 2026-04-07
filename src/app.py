8171d4d70cd47157a93b8c1dacd88e4844a67dc4
orugantipavan <orugantipavan21@gmail.com>
2026-04-07 15:14:10 +0530

    Fix port binding for Render depolyment

--- src/app.py
+++ src/app.py
@@ -41,5 +41,8 @@ def predict():
         return jsonify({"error": str(e)})
     
 #Run server
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
