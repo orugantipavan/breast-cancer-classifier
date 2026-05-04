1a85c67809d2739f8aa0ed661a084f12497e0d6c
orugantipavan <orugantipavan21@gmail.com>
2026-05-04 19:04:38 +0530

    Add debug prints

--- src/fastapi_app.py
+++ src/fastapi_app.py
@@ -32,19 +32,22 @@ def home():
 #Prediction route
 @app.post("/predict")
 def predict(data: InputData):
-    #step1: Validate input length
+    #Step 1: Print raw input
+    print("RAW INPUT:", data.features)
+    #step 2: Validate input length
     if len(data.features) != 30:
         return {"error": "Exactly 30 features required"}
     #convert to numpy array
     features = np.array(data.features).reshape(1, -1)
-    #scale features
+    #step 3: Scale features
     features_scaled = scaler.transform(features)
+    print("SCALED INPUT:", features_scaled)
     #Get probabilities
     proba = model.predict_proba(features_scaled)[0]
     #Determine prediction
     prediction = int(proba[1] > 0.5)
     #Get confidence
     confidence = float(proba[prediction])
-    #Step2: Clamp confidence (avoid 100%)
+    #Step 4: Clamp confidence (avoid 100%)
     confidence = min(confidence, 0.999)
     return {"prediction": prediction, "result": "Benign" if prediction == 1 else "Malignant", "confidence": round(confidence * 100, 2)}
