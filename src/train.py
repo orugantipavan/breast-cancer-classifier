b21a4ba8a5b66613d1d93261f9b6939e50c08459
orugantipavan <orugantipavan21@gmail.com>
2026-05-06 14:56:20 +0530

    Fix probability logic

--- src/train.py
+++ src/train.py
@@ -10,23 +10,25 @@ from sklearn.metrics import (accuracy_score, classification_report, confusion_ma
 
 #2.Load dataset
 data=load_breast_cancer()
-X, y = data.data, data.target
+X = data.data
+y = data.target
+
 
 print("Dataset loaded")
 print("Shape:", X.shape)
 
 #3.Train-TestSplit dataset
-X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify = y, random_state=42)
 
 #4.Feature Scaling
 scaler=StandardScaler()
-X_train_s=scaler.fit_transform(X_train)
-X_test_s=scaler.transform(X_test)
+X_train_s=scaler.fit_transform(X_train) #FIT + TRANSFORM
+X_test_s=scaler.transform(X_test) #ONLY TRANSFORM
 
 print("Scaling completed")
 
 #5.Model Training
-model=LogisticRegression(max_iter=5000)
+model=LogisticRegression(max_iter=1000)
 model.fit(X_train_s, y_train)
 
 print("Model trained")
