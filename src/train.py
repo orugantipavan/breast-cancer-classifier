07e6678d34b679f31092cce5026ab1d74cc33b2f
orugantipavan <orugantipavan21@gmail.com>
2026-05-06 18:35:22 +0530

    Added probability calibration

--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.metrics import (accuracy_score, classification_report, confusion_matrix)
 
 #2.Load dataset
@@ -28,7 +29,8 @@ X_test_s=scaler.transform(X_test) #ONLY TRANSFORM
 print("Scaling completed")
 
 #5.Model Training
-model=LogisticRegression(max_iter=1000)
+base_model=LogisticRegression(max_iter=1000)
+model=CalibratedClassifierCV(base_model, method='sigmoid', cv=5)
 model.fit(X_train_s, y_train)
 
 print("Model trained")
