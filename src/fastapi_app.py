47a33735d0c509fa82df04c5aab4d147ec16756c
orugantipavan <orugantipavan21@gmail.com>
2026-05-07 02:05:37 +0530

    Fixed API input structure bug

--- src/fastapi_app.py
+++ src/fastapi_app.py
@@ -22,7 +22,38 @@ scaler=joblib.load("models/scaler.pkl")
 
 #Define input format
 class InputData(BaseModel):
-    features: list[float]
+    mean_radius: float
+    mean_texture: float
+    mean_perimeter: float
+    mean_area: float
+    mean_smoothness: float
+    mean_compactness: float
+    mean_concavity: float
+    mean_concave_points: float
+    mean_symmetry: float
+    mean_fractal_dimension: float
+
+    radius_error: float
+    texture_error: float
+    perimeter_error: float
+    area_error: float
+    smoothness_error: float
+    compactness_error: float
+    concavity_error: float
+    concave_points_error: float
+    symmetry_error: float
+    fractal_dimension_error: float
+
+    worst_radius: float
+    worst_texture: float
+    worst_perimeter: float
+    worst_area: float
+    worst_smoothness: float
+    worst_compactness: float
+    worst_concavity: float
+    worst_concave_points: float
+    worst_symmetry: float
+    worst_fractal_dimension: float
 
 #Home route
 @app.get("/")
@@ -32,23 +63,22 @@ def home():
 #Prediction route
 @app.post("/predict")
 def predict(data: InputData):
-    #Step 1: Print raw input
-    print("RAW INPUT:", data.features)
-    #step 2: Validate input length
-    if len(data.features) != 30:
-        return {"error": "Exactly 30 features required"}
-    #convert to numpy array
-    features = np.array(data.features).reshape(1, -1)
+    #Step 1: Convert input ordered list(VERY IMPORTANT ORDER)
+    features_list = [data.mean_radius, data.mean_texture, data.mean_perimeter, data.mean_area, data.mean_smoothness, data.mean_compactness, data.mean_concavity, data.mean_concave_points, data.mean_symmetry, data.mean_fractal_dimension, data.radius_error, data.texture_error, data.perimeter_error, data.area_error, data.smoothness_error, data.compactness_error, data.concavity_error, data.concave_points_error, data.symmetry_error, data.fractal_dimension_error, data.worst_radius, data.worst_texture, data.worst_perimeter, data.worst_area, data.worst_smoothness, data.worst_compactness, data.worst_concavity, data.worst_concave_points, data.worst_symmetry, data.worst_fractal_dimension]
+    
+    print("RAW INPUT:", features_list)
+    #Step 2: Convert to numpy array
+    features = np.array(features_list).reshape(1, -1)
     #step 3: Scale features
     features_scaled = scaler.transform(features)
     print("SCALED INPUT:", features_scaled)
-    #Get probabilities
+    #Step 4: Predict probability
     proba = model.predict_proba(features_scaled)[0]
     print("PROBABILIETIES:", proba)
-    #Determine prediction
+    #Step 5: Decision
     prediction = int(proba[1] > 0.5)
     #Get confidence
     confidence = float(proba[prediction])
-    #Step 4: Clamp confidence (avoid 100%)
+    #Step 6: Clamp confidence (avoid 100%)
     confidence = min(confidence, 0.999)
     return {"prediction": prediction, "result": "Benign" if prediction == 1 else "Malignant", "confidence": round(confidence * 100, 2)}
